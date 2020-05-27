bf2329d45bebab56bb290a3fd9cc7eaba0abf294
patam123 <patrik.hammarstrand@gmail.com>
2020-05-27 15:50:50 +0200

    correction of nav.js

---
+++ src/nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export function navigate(first,second,third)
+{
+    
+    return (
+        <ul>
+        <li>>{first}</li>
+        <li>{second}</li>
+        <li>{third}</li>
+        </ul>
+        )
+};
