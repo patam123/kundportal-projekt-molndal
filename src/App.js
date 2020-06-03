244c9cdf61ed0c2dc6bb541932303d941196c233
patam123 <patrik.hammarstrand@gmail.com>
2020-06-03 10:37:02 +0200

    fix header-component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import {} from './components/nav.js';
 import {section} from './components/section.js';
-import {header} from './components/header.js';
+// import {header} from './components/header.js';
 import {navigate} from './components/nav.js'
 import Profile from "./components/profile";
 import Home from "./components/home";
--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
+import ButtonComponent from "./button";
 
 //verkar funka
-function header(text)
-{
+class Header extends React.Component {
 
+  render() {
     return (
-        
-        <div className="header-layout">{text}</div>
-
-        )
+      <div className="header-layout">
+        <h3>{this.props.headText}</h3>
+        <ButtonComponent
+            className="header-button"
+          isClicked={this.props.handleBtnClick}
+          btnText={this.props.btnText}
+        />
+      </div>
+    );
+  }
 }
-
-export default header;
+export default Header;
