98c278687d7596f1662a483eed74fed9623fa371
peterpham199 <55827530+peterpham199@users.noreply.github.com>
2020-06-03 10:20:26 +0200

    Card created
    Create a card component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {header} from './components/header.js';
 import {navigate} from './components/nav.js'
 import Profile from "./components/profile";
 import Home from "./components/home";
+import Card from "./components/card"
 
 import './App.css';
 
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
     <Home title="Home"/>
     {navigate("1","2","3")}
+    <Card/>
     {section}
     {section}
     <Profile />
