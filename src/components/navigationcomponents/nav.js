d96ae10eef079c0fe77196b38537182d0d82b462
peterpham199 <55827530+peterpham199@users.noreply.github.com>
2020-06-06 21:12:24 +0200

    Created navigation
    Created links and css. Navigation is almost done.

--- src/components/navigationcomponents/nav.js
+++ src/components/navigationcomponents/nav.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 
-function navigate(first,second,third)
-{
+function navigation({ path, name, navstyle, icon, handleMouseOver,handleMouseLeave,handleClick, keyId }) {
+  const linkStyle = {
+    fontSize: "1vw",
+    fontFamily: "Arial",
     
-    return (
-        <ul>
-        <li>>{first}</li>
-        <li>{second}</li>
-        <li>{third}</li>
-        </ul>
-        )
-};
+  };
+  
+  return (    
+        <div >   
+        <Link style={linkStyle} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} onClick={handleClick} className={navstyle} id={keyId} to={path}>
+          <div id={icon}></div>
+          {name}
+        </Link>       
+        </div>      
+  );
+}
 
-export {navigate}
+export default navigation;
