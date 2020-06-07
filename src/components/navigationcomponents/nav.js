a094ea96a1932407e39ddfd7dfb1bc5e2b5e55d8
peterpham199 <55827530+peterpham199@users.noreply.github.com>
2020-06-07 19:15:24 +0200

    fix links css bug

--- src/components/navigationcomponents/nav.js
+++ src/components/navigationcomponents/nav.js
@@ -11,12 +11,12 @@ function navigation({ path, name, navstyle, icon, handleMouseOver,handleMouseLea
   };
   
   return (    
-        <div >   
+         
         <Link style={linkStyle} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} onClick={handleClick} className={navstyle} id={keyId} to={path}>
           <div id={icon}></div>
           {name}
         </Link>       
-        </div>      
+           
   );
 }
 
