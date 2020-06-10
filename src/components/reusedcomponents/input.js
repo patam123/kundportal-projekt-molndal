93a71618d8bfecb5adeee7d63e1325c949a9b5de
peterpham199 <55827530+peterpham199@users.noreply.github.com>
2020-06-10 13:05:51 +0200

    fix navigation and little setting profile css

--- src/components/reusedcomponents/input.js
+++ src/components/reusedcomponents/input.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function input({ inputtype, inputname, inputvalue, labelname }) {
+function input({ inputtype, inputname, inputvalue, labelname,inputStyle }) {
   return (
     <div>
       <div>
         <label style={{color: "#A1A8C3"}}>{labelname}</label>
         <br></br>
-        <input type={inputtype} name={inputname} value={inputvalue}></input>
+        <input id={inputStyle} type={inputtype} name={inputname} value={inputvalue}></input>
       </div>
     </div>
   );
--- src/components/settingcomponents/profilesetting.js
+++ src/components/settingcomponents/profilesetting.js
@@ -1,61 +1,81 @@
 import React from "react";
 import Input from "../reusedcomponents/input";
 import Button from "../reusedcomponents/button";
-
+import ProfilePhoto from "../reusedcomponents/profile-photo";
+import User from "../usercomponents/user";
+import SettingProfileStyle from "../../design/settingprofileStyle.css"
 
 function profileSetting() {
+  const user = new User(
+    "Example",
+    "Person",
+    "19800505-5555",
+    "example.person@gmail.com",
+    "0700000000",
+    "Landsvägen 1",
+    "40010",
+    "Göteborg"
+  );
   return (
-    <div>
-     
-      <Input
-        inputtype="text"
-        inputName="Peter"
-        inputvalue="Peter"
-        labelname="Förnamn"
-      />
-      <Input
-        inputtype="text"
-        inputName="Peter"
-        inputvalue="Peter"
-        labelname="Efternamn"
-      />
-      <Input
-        inputtype="text"
-        inputName="Peter"
-        inputvalue="Peter"
-        labelname="Personnummer"
-      />
-      <Input
-        inputtype="text"
-        inputName="Peter"
-        inputvalue="Peter"
-        labelname="Adress"
-      />
-      <Input
-        inputtype="text"
-        inputName="Peter"
-        inputvalue="Peter"
-        labelname="Postort"
-      />
-      <Input
-        inputtype="text"
-        inputName="Peter"
-        inputvalue="Peter"
-        labelname="Postnummer"
-      />
-      <Input
-        inputtype="text"
-        inputName="Peter"
-        inputvalue="Peter"
-        labelname="Telefonnummer"
-      />
-      <Input
-        inputtype="text"
-        inputName="Peter"
-        inputvalue="Peter"
-        labelname="email"
-      />
+    <div id="settingProfileStyle">
+    <div id="photo-style">{<ProfilePhoto user={user} />}</div>
+      <div id="input-Container-Style">    
+        <div id="first-last-NameContainer">
+          <Input
+            inputStyle="firstname-style"
+            inputtype="text"
+            inputName="Peter"
+            inputvalue="Peter"
+            labelname="Förnamn"
+          />
+          <Input
+          inputStyle="lastname-style"
+            inputtype="text"
+            inputName="Peter"
+            inputvalue="Peter"
+            labelname="Efternamn"
+          />
+        </div>
+
+        <Input
+          inputtype="text"
+          inputName="Peter"
+          inputvalue="Peter"
+          labelname="Personnummer"
+        />
+        <Input
+          inputtype="text"
+          inputName="Peter"
+          inputvalue="Peter"
+          labelname="Adress"
+        />
+        <Input
+          inputtype="text"
+          inputName="Peter"
+          inputvalue="Peter"
+          labelname="Postort"
+        />
+        <Input
+          inputtype="text"
+          inputName="Peter"
+          inputvalue="Peter"
+          labelname="Postnummer"
+        />
+        <Input
+          inputtype="text"
+          inputName="Peter"
+          inputvalue="Peter"
+          labelname="Telefonnummer"
+        />
+        <Input
+          inputtype="text"
+          inputName="Peter"
+          inputvalue="Peter"
+          labelname="Email"
+        />
+      </div>
       <Button btnText="Spara" />
+      
     </div>
   );
 }
