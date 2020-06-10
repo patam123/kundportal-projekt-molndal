b971a99247880398f8477d3779af993594af8905
peterpham199 <55827530+peterpham199@users.noreply.github.com>
2020-06-10 18:18:04 +0200

    setting css

--- src/components/reusedcomponents/input.js
+++ src/components/reusedcomponents/input.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function input({ inputtype, inputname, inputvalue, labelname,inputStyle }) {
+function input({ inputtype, inputvalue, labelname,inputStyle }) {
   return (
     <div>
       <div>
         <label style={{color: "#A1A8C3"}}>{labelname}</label>
         <br></br>
-        <input id={inputStyle} type={inputtype} name={inputname} placeholder={inputvalue}></input>
+        <input id={inputStyle} type={inputtype} placeholder={inputvalue}></input>
       </div>
     </div>
   );
