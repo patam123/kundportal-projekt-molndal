7bd030eb26cf81b294d67efae5194a58738a4bf7
patam123 <patrik.hammarstrand@gmail.com>
2020-06-15 17:23:08 +0200

    update sql

--- api/sql/kundportal-mysql-dml.sql
+++ api/sql/kundportal-mysql-dml.sql
@@ -2,7 +2,7 @@ INSERT INTO Company (Name, ShareWorth, ShareNumber, Industry) VALUES('Volvo', 5.
 
 INSERT INTO Company (Name, ShareWorth, ShareNumber, Industry) VALUES('Ericsson', 0.44, '00000000', 'Telekom');
 
-INSERT INTO Customer(Email, Password, FirstName, LastName, PersonNumber, Address, PostCode, PostAddress, PhoneNumber, ProfilePicture) VALUES('example.person@example.com', 'example123', 'Example', 'Person', '198005051633', 'Vägen 1', '41501', 'Göteborg', '0700000000', 'Insert pic here');
+INSERT INTO Customer(Email, Password, FirstName, LastName, PersonNumber, Address, PostCode, PostAddress, PhoneNumber, ProfilePicture) VALUES('example.person@example.com', 'example123', 'Example', 'Person', '198005051633', 'Vï¿½gen 1', '41501', 'Gï¿½teborg', '0700000000', 'Insert pic here');
 
 
 
@@ -10,10 +10,8 @@ INSERT INTO Share(CompanyId, Owner, Type) VALUES((SELECT Id FROM Company WHERE I
 
 
 
-
-SELECT * FROM Share
-
-;
+SELECT s.Type as Type, co.Name as Name, co.Industry as Industry, co.ShareNumber as ShareNumber, co.ShareWorth as ShareWorth FROM Share s INNER JOIN Company co ON s.CompanyId = co.Id WHERE s.Owner = 1;
+SELECT * FROM Share;
 SELECT * FROM Company;
 
 SELECT Email, FirstName, LastName, PersonNumber, Address, PostCode, PostAddress, PhoneNumber FROM Customer WHERE Id=2;
